7014d4ec88d932e6e4da2d1d79916207b74592a7
Andrew Huang <huanga2@student.unimelb.edu.au>
2020-09-14 22:21:37 +1000

    added string conversion

--- mur_stereo_pipeline/includes/StereoCommon/ConeColorID.hpp
+++ mur_stereo_pipeline/includes/StereoCommon/ConeColorID.hpp
@@ -1,7 +1,16 @@
 #pragma once
+#include <iostream>
 
 enum class ConeColorID {
     Blue = 0,
     Orange = 1,
     Yellow = 2
 };
+
+inline std::string ConeColorID2str(ConeColorID coneColorID) {
+    switch (coneColorID) {
+        case (ConeColorID::Blue)    : return "Blue";
+        case (ConeColorID::Orange)  : return "Orange";
+        case (ConeColorID::Yellow)  : return "Yellow";
+    }
+}
